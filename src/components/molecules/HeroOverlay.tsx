5eb0dc259c6edc715ba829502d1dffe0b33a564f
Riyad Osama <riyadosama14112000@gmail.com>
2026-01-15 15:43:54 +0200

    feat: add HeroOverlay components

--- src/components/molecules/HeroOverlay.tsx
+++ src/components/molecules/HeroOverlay.tsx
@@ -1,13 +1,49 @@
+import Circle from "../atoms/Circle";
 import Hexagonal from "../atoms/Haxagonal";
+import Square from "../atoms/Square";
+import Rectangle from "../atoms/Rectangle";
+import Wavy from "../atoms/Wavy";
 
 export default function HeroOverlay() {
   return (
-    <>
-      <svg width="95vw" height="95vh" xmlns="http://www.w3.org/2000/svg">
-        <g transform="translate(50,50)">
-          <Hexagonal text="POST TEAMWORK" />
+    <div className="h-screen w-full overflow-hidden">
+      <svg viewBox="0 0 1200 800" preserveAspectRatio="xMidYMid" className="h-full w-full">
+        <g>
+          <g transform="translate(50, 50) rotate(-10 100 100)">
+            <Square text="WORK WITH PICS" />
+          </g>
+
+          <g transform="translate(100, 180)">
+            <Circle text="PDF • AD BLOCKER • VPN •" />
+          </g>
+
+          <g transform="translate(0, 350)">
+            <Hexagonal text="BOOST TEAMWORK" />
+          </g>
+
+          <g transform="translate(50, 500)">
+            <Wavy text="HEIC TO JPG • CONVERT •" />
+          </g>
+        </g>
+
+        <g>
+          <g transform="translate(950, 50) rotate(15 100 100)">
+            <Rectangle text="FIX WIFI" />
+          </g>
+
+          <g transform="translate(850, 200) rotate(-5 100 100)">
+            <Square text="PLAN YOUR DAY" />
+          </g>
+
+          <g transform="translate(1000, 300)">
+            <Wavy text="HEIC TO JPG • CONVERT •" />
+          </g>
+
+          <g transform="translate(900, 450) rotate(10 100 100)">
+            <Rectangle text="MANAGE YOUR MAC" />
+          </g>
         </g>
       </svg>
-    </>
+    </div>
   );
 }

c0f7b477168c2fa33b8f46e8c3abab60b0345d34
Riyad Osama <riyadosama14112000@gmail.com>
2026-01-15 06:39:20 +0200

    feat: add Wavy svg

--- src/components/molecules/HeroOverlay.tsx
+++ src/components/molecules/HeroOverlay.tsx
@@ -1,11 +1,11 @@
-import Rectangle from "../atoms/Rectangle";
+import Wavy from "../atoms/Wavy";
 
 export default function HeroOverlay() {
   return (
     <>
       <svg width="95vw" height="95vh" xmlns="http://www.w3.org/2000/svg">
         <g transform="translate(50,50)">
-          <Rectangle text="MAKE YOUR MAC" />
+          <Wavy text="CONVERT HEIC TO JPG •" />
         </g>
       </svg>
     </>
